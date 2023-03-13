d5701414b7235f04adc190bec148a19d16e52e11
stiven <stivencuestamena@gmail.com>
2023-03-12 23:57:54 -0500

    master

--- Secundaria1.2/cd.js
+++ Secundaria1.2/cd.js
@@ -1,14 +1,17 @@
-$(document).ready(function() {
-    // mostrar submenú al hacer clic en elemento con clase dropdown
-    $('.dropdown').click(function() {
-      $(this).find('.submenu').slideToggle('fast');
-    });
-    // ocultar submenú al hacer clic fuera del menú
-    $(document).click(function(event) {
-      var target = $(event.target);
-      if (!target.closest('.dropdown').length) {
-        $('.submenu').slideUp('fast');
-      }
-    });
-  });
+
   
+  const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Remove active class from all links
+    menuLinks.forEach((link) => {
+      link.classList.remove('active');
+    });
+
+    // Add active class to clicked link
+    link.classList.add('active');
+  });
+});
