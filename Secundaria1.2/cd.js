f34ecfdb6ba180753aa0413acbcc885cea3c8042
stiven <stivencuestamena@gmail.com>
2023-03-14 16:35:53 -0500

    master

--- Secundaria1.2/cd.js
+++ Secundaria1.2/cd.js
@@ -4,14 +4,23 @@
 
 menuLinks.forEach((link) => {
   link.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // Remove active class from all links
     menuLinks.forEach((link) => {
-      link.classList.remove('active');
-    });
+      link.classList.remove('active')
+    })
 
     // Add active class to clicked link
-    link.classList.add('active');
-  });
-});
+    link.classList.add('active')
+  })
+})
+
+
+/* Función para desplegar el menú en pantallas pequeñas */
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const menuItems = document.querySelector('.menu-items');
+
+hamburgerIcon.addEventListener('click', () => {
+  menuItems.classList.toggle('active')
+})
